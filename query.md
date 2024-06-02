b8f71f1176d67376a3c331fb6b913de4175d17f2
mingke-utas <95258603+Mingke1999@users.noreply.github.com>
2024-06-02 21:53:17 +1000

    mutation CUD

--- query.md
+++ query.md
@@ -15,4 +15,55 @@ query ExampleQuery($id:ID!){
     {
         "id":"1"
         }
+query ReviewQuery($id:ID!){
+  review(id:$id){
+    rating,
+    game{
+      title,
+      platform,
+      reviews {
+        rating,
+        content
+      }
+    },
+    author {
+      name,
+      verified
+    }
+  }
+}
+
+mutation deleteMutation($id:ID!){
+  deleteGame(id:$id){
+    id,
+    title,
+    platform
+  }
+}
+
+mutation AddMutation($game:AddGameInput!){
+  addGame(game:$game){
+    id,
+    title,
+    platform
+  }
+}
+{
+  "game":{
+    "title":"state of decay 2",
+    "platform":"xbox"
+  }
+}
+mutation EditMutation($edits:EditGameInput!, $id:ID!){
+  updateGame(edits:$edits, id:$id){
+    title,
+    platform
+  }
+}
+{
+  "edits":{
+    "title":"state of decay 2",
+  },
+  "id":"3"
+}
 ```
--- server.js
+++ server.js
@@ -38,6 +38,30 @@ const resolvers = {
         game(parent){
             return db.games.find((game)=> game.id === parent.game_id)
         }
+    },
+    Mutation:{
+        deleteGame(_, args){
+            db.games = db.games.filter((game) => game.id !== args.id)
+            return db.games
+        },
+        addGame(_,args){
+            let game = {
+                ...args.game,
+                id:Math.floor(Math.random()*1000).toString()
+            }
+
+            db.games.push(game)
+            return game
+        },
+        updateGame(_, args){
+            db.games = db.games.map((game)=> {
+                if(game.id === args.id){
+                    return {...game, ...args.edits}
+                }
+                return game
+            })
+            return db.games.find((game)=> game.id === args.id)
+        }
     }
 }
 
